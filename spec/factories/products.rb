345d5c418180dbd59691ef6a45412ed3d249fe9f
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-12 18:41:52 +0900

    テストコードの実装

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :product do
-    
+    product_name       {Faker::Name}
+    category_id        {Faker::Number.between(from: 2, to: 11)}
+    condition_id       {Faker::Number.between(from: 2, to: 7)}
+    description        {Faker::Lorem.sentence}
+    delivery_charge_id {Faker::Number.between(from: 2, to: 3)}
+    price              {Faker::Number.between(from: 300, to: 9_999_999)}
+    delivery_time_id   {Faker::Number.between(from: 2, to: 4)}
+    ship_form_id       {Faker::Number.between(from: 2, to: 48)}
+    association :user
+
+    after(:build) do |product|
+      product.image.attach(io: File.open("public/images/test_image.png"), filename: "test_image.png")
+    end
   end
 end
