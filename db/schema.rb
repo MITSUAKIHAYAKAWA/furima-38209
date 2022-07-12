fd780c0dccaed0e261603b5ff2d592c06dc46091
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-12 11:23:07 +0900

    productモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_011128) do
+ActiveRecord::Schema.define(version: 2022_07_12_021409) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name"
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.text "description"
+    t.integer "price"
+    t.integer "delivery_time_id"
+    t.integer "delivery_charge_id"
+    t.integer "ship_form_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_07_11_011128) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
