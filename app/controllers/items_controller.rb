1724ba1e6739c9f30f165f6393bccacb329bf5d5
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-13 19:52:59 +0900

    商品編集ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @products = Product.all.order('id DESC')
@@ -19,8 +21,19 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to item_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
 
   private
 
@@ -28,4 +41,15 @@ class ItemsController < ApplicationController
     params.require(:product).permit(:product_name, :image, :description, :category_id, :condition_id, :delivery_charge_id,
                                     :ship_form_id, :delivery_time_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @product.user.id
+      redirect_to root_path
+    end
+  end
+
 end
