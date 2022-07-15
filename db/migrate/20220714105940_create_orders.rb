75aea3a7db9bee771380a00476f90546edb150a1
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-15 15:28:58 +0900

    マイグレーションファイルの修正

--- db/migrate/20220714105940_create_orders.rb
+++ db/migrate/20220714105940_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.references :user
+      t.references :product
       t.timestamps
     end
   end
--- db/migrate/20220714110019_create_addresses.rb
+++ db/migrate/20220714110019_create_addresses.rb
@@ -1,6 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.integer   :prefecture_id
+      t.string    :city
+      t.string    :block
+      t.string    :building
+      t.string    :postcode
+      t.string    :phone_number
+      t.references :order, foreign_key: true
 
       t.timestamps
     end
