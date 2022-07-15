8ce1bdaee8a90fb55a0774ddaff2873d2072f86f
MITSUAKIHAYAKAWA <uiharu.tokotke929@gmail.com>
2022-07-15 19:06:00 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,35 @@
 class OrdersController < ApplicationController
 
   def index
+    @product = Product.find(params[:item_id])
     @user_investment = UserInvestment.new
   end
+
+  def create
+    binding.pry
+    @user_investment = UserInvestment.new(order_params)
+    if @user_investment.valid?
+      @user_investment.save
+      # pay_item
+      redirect_to root_path
+    else
+      @product = Product.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:user_investment).permit(:prefecture_id, :city, :block, :building, :postcode, :phone_number).merge(user_id: current_user.id, product_id: params[:item_id])
+  end
+
+  # def pay_item
+    # Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      # Payjp::Charge.create(
+        # price: order_params[:id],
+        # card: order_params[:token],    
+        # currency: 'jpy'                 
+      # )
+  # end
 end
--- app/models/user_investment.rb
+++ app/models/user_investment.rb
@@ -1,7 +1,7 @@
 class UserInvestment
 
   include ActiveModel::Model
-  attr_accessor :prefecture_id, :city, :block, :building, :postcode, :phone_number, :user_id, :product_id
+  attr_accessor :prefecture_id, :city, :block, :building, :postcode, :phone_number, :user_id, :product_id, :order_id
   
   with_options presence: true do
     validates :prefecture_id, numericality: { other_than: 1 }
@@ -9,14 +9,13 @@ class UserInvestment
     validates :block
     validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/ }  
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-    validates :token
+    validates :order_id
   end
   validate :building
   
 
   def save
-    Order.create(user_id: current_user.id, product_id: params[:product.id])
-    Address.create(postal: postal, prefecture_id: prefecture_id, city: city, block: block,
-                    building: building, phone: phone, user_id: current_user.id )
+    order = Order.create(user_id: user_id, product_id: product_id )
+    Address.create(prefecture_id: prefecture_id, city: city, block: block, building: building, postcode: postcode, phone_number: phone_number, order_id: product_id)
   end
 end
